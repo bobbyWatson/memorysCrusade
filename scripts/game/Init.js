377ec6dec0532157dc9127d14550d5f7df005ea0
wilkoJ <perruchon.wilko.isart@gmail.com>
2013-12-11 08:11:03 +0100

    ajout des flemmes

--- scripts/game/Init.js
+++ scripts/game/Init.js
@@ -1,5 +1,5 @@
-define(["Game", "AssetsController","Run", "MovingPlateform", "BouncingBall", "Player", "GrabPoint", "B2D", "Ladder", "StaticPlateform", "Camera", "Spawn", "Background", "LevelController"], 
-	function (Game, AssetsController, Run, MovingPlateform, BouncingBall, Player, GrabPoint, Box2D, Ladder, StaticPlateform, Camera, Spawn, Background, LevelController){
+define(["Game", "AssetsController","Run", "Cube", "Fire","MovingPlateform", "BouncingBall", "Player", "GrabPoint", "B2D", "Ladder", "StaticPlateform", "Camera", "Spawn", "Background", "LevelController"], 
+	function (Game, AssetsController, Run, Cube,Fire ,MovingPlateform, BouncingBall, Player, GrabPoint, Box2D, Ladder, StaticPlateform, Camera, Spawn, Background, LevelController){
 	return function Init(){
 		AssetsController.loadImages();
 		AssetsController.waitForImagesLoaded();
@@ -8,6 +8,8 @@ define(["Game", "AssetsController","Run", "MovingPlateform", "BouncingBall", "Pl
 			LevelController.createLevel(AssetsController.images.level);
 			Game.gameObjects.push(new Background({img : AssetsController.images.level}));
 			Game.gameObjects.push(new StaticPlateform({x : 30, y : 19, width : 45, height : 0.5}));
+			Game.gameObjects.push(new Fire({x : 50, y : 16, width : 4, height : 3}));
+			// Game.gameObjects.push(new Cube({x : 25, y : 12, width : 2, height : 2}));
 			Game.gameObjects.push(new StaticPlateform({x : 10, y : 15, width : 1, height : 25}));
 			Game.gameObjects.push(new MovingPlateform({x : 45, y : 20,  height : 1, width:5, dist:2, speed:4, Vertical:true}));
 			Game.gameObjects.push(new MovingPlateform({x : 15, y : 25,  height : 1, width:5, dist:2, speed:4, Vertical:false}));
---
+++ scripts/game/functions/killPlayer.js
@@ -0,0 +1,18 @@
+define(["Game"], function (Game){
+
+	return function killPlayer( args ){
+		var otherCollider;
+		if(args.m_fixtureA.GetBody().GetUserData().id === this.id){
+			otherCollider = args.m_fixtureB;
+		} else{
+			otherCollider = args.m_fixtureA;
+		}
+
+		if(otherCollider.GetBody().GetUserData().tag === "Player"){
+		console.log(otherCollider.GetBody().GetUserData().spawn);
+		console.log(otherCollider.GetBody().GetUserData());
+		console.log(otherCollider.GetBody().GetUserData().death);
+			otherCollider.GetBody().GetUserData().death(otherCollider.GetBody().GetUserData().spawn);
+		}
+	}
+})
